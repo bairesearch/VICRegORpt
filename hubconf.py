fd002a471410cf7512ca8d46720e8b6bed8adfa4
Richard Baxter <richard.baxter@baxterai.com>
2023-05-01 00:30:04 +1000

    apply tab indentation

--- hubconf.py
+++ hubconf.py
@@ -12,33 +12,33 @@ dependencies = ["torch", "torchvision"]
 
 
 def resnet50(pretrained=True, **kwargs):
-    model, _ = resnet.resnet50(**kwargs)
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url="https://dl.fbaipublicfiles.com/vicreg/resnet50.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-    return model
+	model, _ = resnet.resnet50(**kwargs)
+	if pretrained:
+		state_dict = torch.hub.load_state_dict_from_url(
+			url="https://dl.fbaipublicfiles.com/vicreg/resnet50.pth",
+			map_location="cpu",
+		)
+		model.load_state_dict(state_dict, strict=True)
+	return model
 
 
 def resnet50x2(pretrained=True, **kwargs):
-    model, _ = resnet.resnet50x2(**kwargs)
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url="https://dl.fbaipublicfiles.com/vicreg/resnet50x2.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-    return model
+	model, _ = resnet.resnet50x2(**kwargs)
+	if pretrained:
+		state_dict = torch.hub.load_state_dict_from_url(
+			url="https://dl.fbaipublicfiles.com/vicreg/resnet50x2.pth",
+			map_location="cpu",
+		)
+		model.load_state_dict(state_dict, strict=True)
+	return model
 
 
 def resnet200x2(pretrained=True, **kwargs):
-    model, _ = resnet.resnet200x2(**kwargs)
-    if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(
-            url="https://dl.fbaipublicfiles.com/vicreg/resnet200x2.pth",
-            map_location="cpu",
-        )
-        model.load_state_dict(state_dict, strict=True)
-    return model
+	model, _ = resnet.resnet200x2(**kwargs)
+	if pretrained:
+		state_dict = torch.hub.load_state_dict_from_url(
+			url="https://dl.fbaipublicfiles.com/vicreg/resnet200x2.pth",
+			map_location="cpu",
+		)
+		model.load_state_dict(state_dict, strict=True)
+	return model
